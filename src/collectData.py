e6f9df2b6f223e48ff7bae97966ee260fdcd671b
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2024-11-21 01:08:01 -0500

    fix some changes add SG CTA replication
    Add SG CTA replication as well some backend data additions for futures and FX

--- src/collectData.py
+++ src/collectData.py
@@ -24,6 +24,15 @@ class DataCollector():
         self.parent_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
         self.data_path = os.path.join(self.parent_path, "data")
         
+        self.tickers = {
+            "Commodities": ["CL", "GC", "C", "HG", "S"],
+            "Equities"   : ["ES", "VG", "Z", "NK"],
+            "FX"         : ["EUR", "JPY", "GBP", "CHF"],
+            "Bonds"      : ["TY", "RX", "G", "JB"]}
+        
+        self.fut_path = r"C:\Users\Diego\Desktop\app_prod\BBGFuturesManager\data\PXFront"
+        self.bbg_path = r"C:\Users\Diego\Desktop\app_prod\BBGData\data"
+        
     def _collect_data(self, start_date, end_date, tickers, path):
         
         self.end_date_input  = self.end_date.strftime("%Y%m%d")
@@ -63,8 +72,64 @@ class DataCollector():
         
         print("Collected SPX and SPX Futures")
         
-if __name__ == "__main__":
+    def collect_futures(self, verbose: bool = False) -> pd.DataFrame:
+        
+        file_path = os.path.join(self.data_path, "FuturesData.parquet")
+        try:
+            
+            if verbose == True: print("Trying to find the futures data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find data, now collecting it")
+            
+            tickers = [
+                os.path.join(self.fut_path, item + ".parquet") 
+                for key, val in self.tickers.items() 
+                if key != "FX" for item in val]
+    
+            df_out = (pd.read_parquet(
+                path = tickers, engine = "pyarrow"))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_out
+    
+    def collect_fx(self, verbose: bool = False) -> pd.DataFrame:
+        
+        file_path = os.path.join(self.data_path, "FXData.parquet")
+        try:
+            
+            if verbose == True: print("Trying to find FX data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found data\n")
+            
+        except:
+            
+            if verbose == True: print("Couldn't find FX Data, now collecting it")
+            tickers = ([
+                os.path.join(self.bbg_path, ticker + ".parquet") 
+                for ticker in self.tickers["FX"]])
+            
+            df_out = (pd.read_parquet(
+                path = tickers, engine = "pyarrow").
+                drop(columns = ["variable"]))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_out
+
+def main() -> None: 
     
     data_collector = DataCollector()
     data_collector.collect_hf_indices()
     data_collector.collect_spx()
+    data_collector.collect_futures(verbose = True)
+    DataCollector().collect_fx(verbose = True)
+        
+#if __name__ == "__main__": main()
+    
